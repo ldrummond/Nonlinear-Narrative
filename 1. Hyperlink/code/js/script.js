7503a11e1832bb60b209e222cde6058769b64891
Lucas Drummond <ldrummond@wustl.edu>
2018-02-06 13:18:44 -0600

    working

--- 1. Hyperlink/code/js/script.js
+++ 1. Hyperlink/code/js/script.js
@@ -1,72 +1,56 @@
-var scrap = document.getElementById('text'); 
-var docTitle = document.title;
-var display = document.getElementById('data');
-
-
-
-
-// Sets the title of the document to the image tags alt values. 
-var numLinks;
-var i;
-var compiledTitle = "";
-
-numLinks = document.getElementsByTagName("img");
-
-for (i = 0; i < numLinks.length; i++) {
-    var tempLinkText = document.getElementById('text').childNodes[1].childNodes[0].alt;
-	compiledTitle = compiledTitle + tempLinkText + ", ";
-}
-
-document.title = compiledTitle;
-
+var docHead = document.head
+var fonts = "<link href=\"https://fonts.googleapis.com/css?family=Scheherazade\" rel=\"stylesheet\">"
+var fa = "<script defer src=\"https://use.fontawesome.com/releases/v5.0.6/js/all.js\"></script>"
+docHead.innerHTML = docHead.innerHTML + '\n' + fonts + '\n' + fa 
 
 
 
 var images = document.getElementsByTagName("img");
 var i; 
 
-for (i = 0; i < images.length; i++) {
-    images[i].style.width = images[i].alt.length * 10 + "px";
-    display.innerHTML = images.length;
-}
-
+// Gets the innerHmtl of the P at the center of the page. Splits it by whitespace, then compares it to keywords. 
 var pText = document.getElementById("text").innerHTML;
 var pTextArray = pText.split(" ");
-var keyWords = ["clouds", "car"];
-var count = " "; 
+var keyWords = ["clouds", "mud", "plane", "colors", "home", "train", "sea"];
+var count = ""; 
+var pTextEdited = ""; 
 
 for (var i = 0; i < pTextArray.length; i++) {
-	var temp = pTextArray[i]; 
+	var temp = pTextArray[i];
 	for (var j = 0; j < keyWords.length; j++) {
-		if (temp = keyWords[j]) {
-			count = count + temp;   
+		if (pTextArray[i] == keyWords[j]) {
+			var tag = pTextArray[i];
+			var imgWidth = tag.length * 26 + "px"
+			var ranInt = Math.round(Math.random()*9) + 1
+			// temp = "<a href=\"" + tag + ".html\"><img src=\"images/" + tag + ranInt + ".jpg\" alt=\"" + tag + "\" style=\"width:" + imgWidth + ";\"></a>";
+			temp = "<img src=\"images/" + tag + ranInt + ".jpg\" alt=\"" + tag + "\" style=\"width:" + imgWidth + ";\">";
+		} else if (pTextArray[i].substring(0, pTextArray[i].length - 1) == keyWords[j]) {
+			var tag = pTextArray[i].substring(0, pTextArray[i].length - 1);
+			var imgWidth = tag.length * 26 + "px"
+			var ranInt = Math.round(Math.random()*9) + 1
+			temp = "<img src=\"images/" + tag + ranInt + ".jpg\" alt=\"" + tag + "\" style=\"width:" + imgWidth + ";\">";
 		}
 	}
+	pTextEdited += temp + " "; 
 }
+var fileBody = document.body
+fileBody.innerHTML = fileBody.innerHTML + '\n' + "<a href=\"source.html\"><h4 id=\"source\">source</h4></a>"
 
-display.innerHTML = count;
-
-
-
-
-
-
+document.getElementById("text").innerHTML = pTextEdited;
+document.getElementById("text").style.opacity = "1";
+// document.getElementById("text").style.fontSize =  + "px";
 
 
-// document.title = compiledTitle; 
+// Sets the title of the document to the image tags alt values. 
+// var numLinks;
+// var i;
+// var compiledTitle = "";
 
-// var linkText = document.getElementById('this').childNodes[1].text;
-// var linkTextLength = linkText.length;
-// display.innerHTML = linkText + " " + linkTextLength;
-// display.style.width = "400px";
+// numLinks = document.getElementsByTagName("img");
 
-// var imageTag = document.getElementById('this').childNodes[1].childNodes[0];
-// // imageTag.style.opacity = "0";
-// imageTag.style.width = linkTextLength*10 + "px";
-// // imageTag.style.height = document.getElementById('this').style.fontSize; 
+// for (i = 0; i < numLinks.length; i++) {
+//     var tempLinkText = document.getElementById('text').childNodes[1].childNodes[0].alt;
+// 	compiledTitle = compiledTitle + tempLinkText + ", ";
+// }
 
-// display.innerHTML = document.getElementById('this').style.fontSize + "px"; 
-
-// docTitle.text = "pleasegod";
-
-// display.innerHTML = "tree";
+// document.title = compiledTitle;
